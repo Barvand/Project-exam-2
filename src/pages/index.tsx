a2fc4a4783e853f314a74adc4b43d946077ee624
B <128028346+Barvand@users.noreply.github.com>
2025-02-12 20:25:26 +0100

    updated styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,8 +21,8 @@ function IndexPage() {
           alt="Holidazing"
         />
         {/* Text Overlay */}
-        <div className="absolute inset-0 flex flex-col items-center justify-center text-white text-center bg-blue-800/40">
-          <h1 className="text-5xl font-bold mb-2">Welcome to Holidaze</h1>
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-black text-center">
+          <h1 className="text-5xl font-bold mb-5">Welcome to Holidaze</h1>
           <h2 className="text-4xl text-accentColor font-semibold">
             Get going, Get Holidazing
           </h2>
